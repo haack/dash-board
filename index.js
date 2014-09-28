5bed705c45bd9cbf1d3235a6cb86a624ce156f8b
Alex <alex_haak@hotmail.co.uk>
2014-09-28 15:08:17 +0100

    Added basic password checking

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var fs = require('fs');
+
+var md5pass = "";
+
+fs.readFile("pass.txt", "ascii", function(error, data) {
+	md5pass = data.slice(0,-1);
+    console.log("Loaded password: " + data);
+});
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('[INFO] user connected');
   socket.on('message', function(data) {
-  	console.log(data);
+  	console.log("\nMessage recieved: " + data + "\n");
+  	if (data === md5pass) {
+  		console.log("[SUCCESS]");
+  	} else {
+  		console.log("[FAIL]");
+  		console.log(md5pass.length + "!=" + data.length);
+  	}
   });
 });
 
