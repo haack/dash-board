89bf370b1c4eb43b8628c487ac7388eb686b332c
Alex <alex_haak@hotmail.co.uk>
2014-09-28 15:19:27 +0100

    Added server-side md5 hashing

--- index.js
+++ index.js
@@ -2,31 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
+var crypto = require('crypto');
 
 var md5pass = "";
 
 fs.readFile("pass.txt", "ascii", function(error, data) {
-	md5pass = data.slice(0,-1);
+	md5pass = data;
     console.log("Loaded password: " + data);
-});
+	
+	app.get('/', function(req, res){
+		res.sendfile('index.html');
+	});
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+	io.on('connection', function(socket){
+		console.log('[INFO] user connected');
+		socket.on('message', function(data) {
+			var hash = crypto.createHash('md5').update(data).digest('hex');
+			console.log("\nHashed pass: " + hash + "\n");
+			if (hash === md5pass) {
+				console.log("[SUCCESS]");
+			} else {
+				console.log("[FAIL]");
+			}
+		});
+	});
 
-io.on('connection', function(socket){
-  console.log('[INFO] user connected');
-  socket.on('message', function(data) {
-  	console.log("\nMessage recieved: " + data + "\n");
-  	if (data === md5pass) {
-  		console.log("[SUCCESS]");
-  	} else {
-  		console.log("[FAIL]");
-  		console.log(md5pass.length + "!=" + data.length);
-  	}
-  });
-});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+	http.listen(3000, function(){
+		console.log('[INFO] listening on *:3000');
+	});    
 });
